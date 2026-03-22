d15dceaa813e4411b7429e9b2fb5013c30747db3
cynthia <cynthia_cui@sfu.ca>
2026-03-22 15:43:26 -0700

    credit to me!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { GateProvider } from "./components/GateProvider";
 import { DrawingProvider } from "./components/DrawingProvider";
 import ScrollContainer from "./components/ScrollContainer";
 
+// DESIGNED AND DEVELOPED BY CYNTHIA CUI @CYNSONLINE ON TWITTER :)
+
 export default function Home() {
   return (
     <GateProvider>
